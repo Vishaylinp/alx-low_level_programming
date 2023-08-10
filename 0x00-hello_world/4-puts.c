0641cc294df1885c8b3c55822a1ae9453195943e
Vish <vishaylinparshotam@gmail.com>
2023-08-10 10:59:33 -0700

    Task 5-7

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+* main - Print the text.
+* Return: 0
+*/
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
