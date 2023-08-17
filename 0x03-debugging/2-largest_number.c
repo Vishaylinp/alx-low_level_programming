739f124cf7b91dd3c899273673f70bd26450191d
Vish <vishaylinparshotam@gmail.com>
2023-08-17 06:41:22 -0700

    error fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && a >= c)
+	if (a > b && a > c)
 	{
 	largest = a;
 	}
-	else if (b >= c && b >= c)
+	else if (c > b && a > c)
 	{
 	largest = c;
 	}
 	else
 	{
-	largest = c;
+	largest = b;
 	}
 
 	return (largest);
