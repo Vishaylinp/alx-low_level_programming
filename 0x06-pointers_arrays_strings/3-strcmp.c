0bc8e1531462dd2f87457c548f5a764b2c8dd819
Vish <vishaylinparshotam@gmail.com>
2023-08-25 03:01:49 -0700

    error fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,9 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[x] != '\0' && s2[x] != '\0')
 	{
-		if (s1[x] < s2[x])
+		if (s1[x] != s2[x])
 		{
-			return (-15);
-		}
-		else if (s1[x] > s2[x])
-		{
-			return (15);
+			return (s1[x] - s2[x]);
 		}
 			x++;
 	}
