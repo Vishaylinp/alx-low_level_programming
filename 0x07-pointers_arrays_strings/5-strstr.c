639f9b0b70c0735390e5962976e3f9f3c69aa6a5
Vish <vishaylinparshotam@gmail.com>
2023-08-29 12:18:11 -0700

    Task complete

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strstr - copies the needle when haystack = needle
+* @haystack: pointer
+* @needle: pointer
+* Return: null or haystack.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	char *x = haystack;
+	char *y = needle;
+
+	for (; *haystack != '\0'; haystack++)
+	{
+		while (*x == *y && *y != '\0')
+		{
+			x++;
+			y++;
+		}
+		if (*y == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
