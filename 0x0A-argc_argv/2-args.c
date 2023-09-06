c37969efe4fa318c2597072f5042a8d5a9bce49c
Vish <vishaylinparshotam@gmail.com>
2023-09-06 08:00:21 -0700

    task done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - entry
+* @argc: count arguement
+* @argv: arguement vector
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; x <= argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
