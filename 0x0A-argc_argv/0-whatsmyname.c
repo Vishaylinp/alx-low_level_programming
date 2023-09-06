66626b55c1d270d0b831499270110e833492285d
Vish <vishaylinparshotam@gmail.com>
2023-09-06 07:27:27 -0700

    Task done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - start point
+* @argv: arguement vector
+* @argc: arguement count
+* Return: 0
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
