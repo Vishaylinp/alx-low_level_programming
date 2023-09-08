45898b3db731a439c677af6435fb5a875b6263ef
Vish <vishaylinparshotam@gmail.com>
2023-09-08 11:52:03 -0700

    task done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* malloc_checked - allocate memory using malloc
+* @unsigned int b: variable
+* Return: pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+	
+	s = malloc(b);
+	if (s == NULL)
+	{
+		exit(98);
+	}
+	return (s);
+}
