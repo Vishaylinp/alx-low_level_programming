776df99f801fa2cb1a0f5bb2e7d5d37543f4b9bc
Vish <vishaylinparshotam@gmail.com>
2023-09-11 05:28:25 -0700

    task done

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+* File : 0-object_like_macro.h
+* Define : size
+*/
+#define SIZE 1024
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,8 @@
+#ifndef PI_H
+#define PI_H
+/**
+* File: 1-pi.h
+* define: PI
+*/
+#define PI 3.14159265359
+#endif
