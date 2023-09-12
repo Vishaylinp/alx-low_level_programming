9bbc6beb5f1a5377a8cc3884ef60031fd906270c
Vish <vishaylinparshotam@gmail.com>
2023-09-12 09:01:05 -0700

    Task done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+* struct dog - information of a dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+*
+* Description: dog identity
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
