5107918b795e2d8c2c96653703355661493ff34a
Vish <vishaylinparshotam@gmail.com>
2023-09-13 09:25:47 -0700

    d

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,8 +7,12 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t  x;
-
+	unsigned int x;
+	
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
 	for (x = 0; x < size; x++)
 	{
 		action(array[x]);
