f909f88200ed00accdc9a400acbd72dc06fe1f75
Vish <vishaylinparshotam@gmail.com>
2023-09-13 12:15:53 -0700

    task done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
 /**
-* int_index - index
+* int_index - returns the index
 * @array: array
 * @size: size of array
 * @cmp: compare
+* Return: x
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
