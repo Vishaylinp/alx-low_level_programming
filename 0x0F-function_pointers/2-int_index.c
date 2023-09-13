dcc110a8f41c4a688fe59b97eff22090c4f231bf
Vish <vishaylinparshotam@gmail.com>
2023-09-13 12:10:06 -0700

    task done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,14 +8,14 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int x;
-	
-	if (array == NULL || cmp == NULL || size <= 0)
+
+	if (array == NULL  || size <= 0 || cmp == NULL)
 	{
 		return (-1);
 	}
 	for (x = 0; x < size; x++)
 	{
-		if (cmp(array[x]) != 0)
+		if (cmp(array[x]))
 		{
 			return (x);
 		}
