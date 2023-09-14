c2c07fa657f59386d1b3999bd583dfd957be94a7
Vish <vishaylinparshotam@gmail.com>
2023-09-14 07:54:29 -0700

    task done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,7 @@
+#ifndef _VARIADIC FUNCTION_
+#define _VARIADIC FUNCTION_
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
