f57571962b66c2f5601341c305b1173905201c98
Vish <vishaylinparshotam@gmail.com>
2023-09-15 08:36:08 -0700

    task done

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+/**
+* print_strings - combine strings together
+* @separator: comma
+* @n: number of string
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	char *s;
+	
+	va_list ag;
+
+	va_start(ag, n);
+
+	for (x = 0; x < n; x++)
+	{
+		s  = va_arg(ag ,char*);
+		
+		if (s == NULL)
+		{
+			printf("nil");
+		}
+		else
+			printf("%s", s);
+
+		if (separator != NULL && x != (n -1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ag);
+}
+
+
