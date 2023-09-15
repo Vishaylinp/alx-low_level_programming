42c523a2272ee848962eacb6a118ce93ba43b364
Vish <vishaylinparshotam@gmail.com>
2023-09-15 07:23:27 -0700

    Task done

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+* print_numbers - print number set.
+* @seperator: seperates text with comma
+* Return: numbers
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	int print = 0;
+
+	va_list ag;
+
+	va_start(ag, n);
+
+	if (separator == NULL)
+	{
+		return;
+	}
+	for (x = 0; x < n; x++)
+	{
+		print = va_arg(ag, int);
+		printf("%d", print);
+		if (x < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(ag);
+	printf("\n");
+}
