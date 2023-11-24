6e9ad52030eec0f66d28f109cf35b116f55039af
Vish <vishaylinparshotam@gmail.com>
2023-11-24 02:32:05 -0800

    Created a function that converts binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary to decimal.
+ * @b - pointer
+ * Return: decimal
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0;
+
+	if (*b == '\0')
+	{
+		return (0);
+	}
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+		{
+			return (0);
+		}
+		else if (*b == '0' || *b == '1')
+		{
+			sum = (sum << 1) + (*b - '0');
+		}
+		else
+			return (0);
+		b++;
+	}
+	return (sum);
+}
