313c5fe8aef5650e6a1cda9ab0339d7fc8d58a15
Vish <vishaylinparshotam@gmail.com>
2023-11-24 07:41:10 -0800

    setbit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* set_bit - set bits
+* @n: number
+* @index: index
+* Return: 1
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bitsval;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	bitsval = (1 << index);
+	*n = *n | bitsval;
+	return (1);
+
+}
+
