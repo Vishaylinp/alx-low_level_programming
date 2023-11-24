6747d657385d241a85570b2d31bbbfa764306ea2
Vish <vishaylinparshotam@gmail.com>
2023-11-24 06:22:23 -0800

    value of bits

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+* get_bit - value of bits
+* @n: number
+* @index: index
+* Return: 0 or 1
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int right = 0;
+	int sum;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	right = (n >> index);
+	sum = right & 1;
+	return (sum);
+
+}
