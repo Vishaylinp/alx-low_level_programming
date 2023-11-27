3e840679aded1dfe4320ac0ad29391d85d1c9683
Vish <vishaylinparshotam@gmail.com>
2023-11-27 06:16:27 -0800

    read file error fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,31 +10,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd;
 	ssize_t rd;
 	ssize_t wr;
-	char *buff = malloc(sizeof(char) * (letters));
+	char *buff = NULL;
 
-	if(buff == NULL)
-	{
-		return (0);
-	}
+
 	if (filename == NULL)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
 	{
-		free(buff);
 		return (0);
 	}
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buff);
-		return (0);
-	}
 	rd = read(fd, buff, letters);
-	if (rd <= 0)
-	{
-		free(buff);
-		return (0);
-	}
 	wr = write(STDOUT_FILENO, buff, rd);
-	if (wr == -1 || wr != rd)
+	if (fd == -1 || rd == -1 || wr == -1 || wr != rd)
 	{
 		free(buff);
 		return (0);
