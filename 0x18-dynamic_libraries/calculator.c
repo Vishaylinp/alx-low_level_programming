3c20ec6745a4d6edc483f450697619c494281a42
Vish <vishaylinparshotam@gmail.com>
2024-01-19 07:27:44 -0800

    run python code with c functions

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+* add - adds two numbers together
+* @x: variable
+* @y: variable
+* Return: sum
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
+/**
+* sub - subtracts two numbers.
+* @x: variable
+* @y: variable
+* Return: subtraction
+*/
+int sub(int x, int y)
+{
+	return (x - y);
+}
+/**
+* mul- multiples two numbers together
+* @x: variable
+* @y: variable
+* Return: multiplication
+*/
+int mul(int x, int y)
+{
+	return (x * y);
+}
+/**
+* div - divides two numbers
+* @x: variable
+* @y: variable
+* Return: division
+*/
+int div(int x, int y)
+{
+	return (x / y);
+}
+/**
+* mod - gets the remainder of two numbers
+* @x: variable
+* @y: variable
+* Return: remainder
+*/
+int mod(int x, int y)
+{
+	return (x % y);
+}
