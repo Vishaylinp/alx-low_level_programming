ac12e5c402cba12faa1f2f9506d9f049c2817d20
Vish <vishaylinparshotam@gmail.com>
2024-02-02 01:42:01 -0800

    create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a hash table
+ * @size: size of table.
+ * Return: pointer to newly created table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *hasht = malloc(sizeof(hash_table_t));
+
+	if (hasht == NULL)
+	{
+		return (NULL);
+	}
+	hasht->size = size;
+	hasht->array = malloc(sizeof(hash_node_t*) * size);
+
+	if (hasht->array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		hasht->array[i] = NULL;
+	}
+	return (hasht);
+}
